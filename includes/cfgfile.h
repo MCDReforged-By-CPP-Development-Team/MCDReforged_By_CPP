65dd124fbb008f795afd866320dea0660ddd9603
3437657364 <3437657364@qq.com>
2020-08-17 15:23:27 +0800

    Minecraft1248 Config File System Update 2020/8/17 15:23

--- includes/cfgfile.h
+++ includes/cfgfile.h
@@ -2,8 +2,14 @@
 cfgfile.h
 包含读取配置文件的类及成员函数声明
 */
+
+#include<string>
+
+using namespace std;
+
 class LoadConfig {
 private:
+	static int ReadCfgFile(string cfgFilePath);
 	static int CreateCfgFile();
 	static bool ConfigFileExisting();
 public:
--- source/cfgfile.cpp
+++ source/cfgfile.cpp
@@ -2,6 +2,8 @@
 #include<Windows.h>
 #include<string>
 
+using namespace std;
+
 bool LoadConfig::ConfigFileExisting() {
     char strCfgPath[MAX_PATH];
     GetModuleFileName(NULL, strCfgPath, MAX_PATH);
@@ -23,14 +25,27 @@ bool LoadConfig::ConfigFileExisting() {
 
 int LoadConfig::LoadConfigFile() {
     if (ConfigFileExisting()) {
+        char strCfgPath[MAX_PATH];
+        GetModuleFileName(NULL, strCfgPath, MAX_PATH);
+        (strrchr(strCfgPath, '\\'))[1] = 0;
+        strcat_s(strCfgPath, "mcdrcppcfg.ini");
+
+        string cfgPath = strCfgPath;
+
+        ReadCfgFile(cfgPath);
         return 0;
     }
     else {
         CreateCfgFile();
+        ReadCfgFile();
         return -1;
     }
 }
 
 int LoadConfig::CreateCfgFile() {
+
+}
+
+int LoadConfig::ReadCfgFile(string cfgFilePath) {
 
 }
