7e4a5e903a8c515f9a4c466f566e64195c8f6e04
xiaoheigithub <xiaoheigithub@users.noreply.github.com>
2020-08-18 20:19:15 +0800

    ixiaohei pushed on 9/18 20:17
    修改了pyplugin.cpp | pyplugin.h
    修改了CmakeList.txt

--- source/cppplugin.cpp
+++ source/cppplugin.cpp
@@ -1,17 +1,21 @@
-#include<cppplugin.h>
-#include<vector>
-#include<Windows.h>
-#include"findfiles.h"
-#include<string>
+//c++ & c headers begin...
+#include <string>
+#include <vector>
+//end; SB Windows Header begin...
+#include <Windows.h>
+//end; Header in Project...
+#include "cppplugin.h"
+#include "findfiles.h"
+//end;
 
 using namespace std;
 
 int CppPluginCore::LoadPlugin() {	//用于加载所有C/C++插件的函数
-	vector<string>PluginFileNames;
+	vector<string> PluginFileNames;
 	char pathBuf[MAX_PATH];
 	GetCurrentDirectoryA(sizeof(pathBuf), pathBuf);
 	string tempStr = pathBuf;
-	string pluginFolderPath =  tempStr + "\\plugins";	//存放插件文件夹路径
+	string pluginFolderPath = tempStr + "\\plugins";	//存放插件文件夹路径
 	GetFiles(pluginFolderPath, CppPluginExtension, PluginFileNames);	//遍历插件文件夹下所有.dll的插件文件
 	if (PluginFileNames.size() <= 0) {
 		return 0;	//后面再来做错误逻辑吧=-=
