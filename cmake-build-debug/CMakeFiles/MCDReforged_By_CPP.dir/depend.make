d3eaee85579d5c7b3babc77489b4f29eb7f99a79
xiaoheigithub <xiaoheigithub@users.noreply.github.com>
2020-08-18 20:47:02 +0800

    ixiaohei pushed on 2020/8/18
    修改了:
      -CmakeLists.txt
      -pyplugin.h
      -pypluhin.cpp

--- cmake-build-debug/CMakeFiles/MCDReforged_By_CPP.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MCDReforged_By_CPP.
-# This may be replaced when dependencies are built.
--- includes/pyplugin.h
+++ includes/pyplugin.h
@@ -1,3 +1,45 @@
-#include"include/Python.h"
+#include <string>
+#include <vector>
 
-#pragma comment(lib, "libs/python37_d.lib")
+#ifndef __MACH__
+#include <Python.h>
+#endif
+
+#ifdef __MACH__
+#include <Python/Python.h>
+#endif
+
+using namespace std;
+
+#define PyPluginExtension ".py" //python²å¼þÀ©Õ¹Ãû
+#define PyPluginDir "pyplugins"
+
+struct PyPlugin {
+    string PluginName = "none";
+    string PluginFileName = "none";
+    string PluginPath = "none";
+    PyObject on_load;
+    PyObject on_unload;
+    PyObject on_info;
+    PyObject on_user_info;
+    PyObject on_player_joined;
+    PyObject on_player_left;
+    PyObject on_death_message;
+    PyObject on_player_made_advancement;
+    PyObject on_server_startup;
+    PyObject on_server_stop;
+    PyObject on_mcdr_stop;
+};
+
+struct PyPluginCore {
+private:
+    void GetPyPluginPath();
+    vector<PyPlugin> PyPlugins;
+public:
+    void LoadAllPyPlugins();
+    void UnLoadAllPyPlugins();
+    void ReLoadAllPyPlugins();
+    void LoadSinglePlugin();
+    void UnloadSinglePlugin();
+    void ReloadSinglePlugin();
+};
