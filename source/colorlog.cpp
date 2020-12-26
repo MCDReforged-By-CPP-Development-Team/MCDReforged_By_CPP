b11a9c7fc2223aa82bb0c2da05a80c9c3f70bbac
arduberrypi <46951736+arduberrypi@users.noreply.github.com>
2020-12-27 02:30:29 +0800

    Add files via upload

---
+++ source/colorlog.cpp
@@ -0,0 +1,23 @@
+#include "colorlog.h"
+
+
+
+ColorLog::ColorLog()
+{
+}
+
+
+int stdfuncallconv ColorLog::out(LPSTR sOut, WCHAR wTextAttribute)
+{
+	HANDLE hOutput;
+	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+	WORD wOldAttribute;
+	DWORD dbWritten;
+	hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hOutput, &csbiInfo);
+	wOldAttribute = csbiInfo.wAttributes;
+	SetConsoleTextAttribute(hOutput, wTextAttribute);
+	WriteFile(hOutput, sOut, lstrlen(sOut), &dbWritten, NULL);
+	SetConsoleTextAttribute(hOutput, wOldAttribute);
+	return dbWritten;
+}
