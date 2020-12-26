57ab05e484c5a76f640a2e6c5235887f1d610d04
arduberrypi <46951736+arduberrypi@users.noreply.github.com>
2020-12-27 02:31:22 +0800

    Add files via upload

---
+++ include/colorlog.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef __COLORLOG
+#define __COLORLOG
+#endif // !COLOLRLOG
+
+#include <iostream>
+#include <string>	
+#include "Windows.h"
+#include "common.h"
+
+using namespace std;
+
+#pragma region COLORS
+#define WHITE 0x07
+#define RED_FOREGROUND FOREGROUND_INTENSITY | FOREGROUND_RED
+#define GREEN_FOREGROUND FOREGROUND_INTENSITY | FOREGROUND_GREEN
+#define BLUE_FOREGROUND FOREGROUND_INTENSITY | FOREGROUND_BLUE
+#define PURPLE_FOREGROUND FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE
+#define YELLOW_FOREGEOUND FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_RED
+#define LIME_FOREGROUND FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE
+//≤ª–¥¡À
+#pragma endregion
+
+
+class ColorLog
+{
+public:
+	ColorLog();	
+	int stdfuncallconv out(LPSTR sOut, WCHAR wTextAttribute);
+
+private:
+
+};
+
