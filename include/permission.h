5469abe35ed8aaa0abaae60dd36d2dc94620d42d
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2021-02-06 12:07:01 +0800

    awa

--- include/permission.h
+++ include/permission.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include<Windows.h>
+
+#include"common.h"
+
+#include"tinyxml2-8.0.0/tinystr.h"  
+#include"tinyxml2-8.0.0/tinyxml.h"
+
+using namespace std;
+
+#define PERMISSION_LEVEL_OWNER 4
+#define PERMISSION_LEVEL_ADMIN 3
+#define PERMISSION_LEVEL_HELPER 2
+#define PERMISSION_LEVEL_USER 1
+#define PERMISSION_LEVEL_GUEST 0
+
+#define PERMISSION_FILE_NAME
+
+int stdfuncallconv ReadPermFile();
