a915decc5c7c30c415811c36177678893b174666
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2021-02-06 09:46:52 +0800

    something

--- source/cmdsys.cpp
+++ source/cmdsys.cpp
@@ -8,6 +8,10 @@ int stdfuncallconv MCDRCommand::ExecCommand()
 	string cmd = this->rawText;
 	string prefix = set.GetString(insprefix);
 
+	if (cmd.find_first_of("!!help") == 0) {
+		this->isexecuted = true;
+		return -1;
+	}
 	if (cmd.find_first_of(prefix) != 0) {
 		this->isexecuted = true;
 		return -1;
@@ -28,6 +32,40 @@ MCDRCommand::MCDRCommand(string command) {
 	this->rawText = command;
 	this->isexecuted = false;
 }
+
+#pragma region CMDs
+
+int stdfuncallconv MCDRCommand::status()
+{
+	return 0;
+}
+
+int stdfuncallconv MCDRCommand::reloads()
+{
+	return 0;
+}
+
+int stdfuncallconv MCDRCommand::permission()
+{
+	return 0;
+}
+
+int stdfuncallconv MCDRCommand::plugins()
+{
+	return 0;
+}
+
+int stdfuncallconv MCDRCommand::mccmdqueue()
+{
+	return 0;
+}
+
+int stdfuncallconv MCDRCommand::help()
+{
+	return 0;
+}
+
+#pragma endregion
 
 int stdfuncallconv NewMCDRCommand(string command)
 {
